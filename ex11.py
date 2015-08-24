3bd79d8b4ddb3cfa3ec67980ecd5d3bb154ad796
maj <maj@witbe.net>
2015-08-24 12:12:49 +0200

    Exercice: 11 (+ extra)

---
+++ ex11.py
@@ -0,0 +1,13 @@
+print("How old are you? ", end = "")
+age = input()
+print("How tall are you? ", end = "")
+height = input()
+print("How much do you weight? ", end = "")
+weight = input()
+
+print("So you're %r old, %r tall and %r heavy." % (age, height, weight))
+
+print("Here we get an int: ", end="")
+an_int = int(input())
+print("The int plus 1: ", end="")
+print(an_int + 1)
---
+++ extra.py
@@ -0,0 +1,6 @@
+import time
+
+
+while True:
+    for s in ["\N{BLACK SPADE SUIT}", "\N{BLACK HEART SUIT}", "\N{BLACK CLUB SUIT}", "\N{BLACK DIAMOND SUIT}"]:
+        print("%s" % s, end = " ")
